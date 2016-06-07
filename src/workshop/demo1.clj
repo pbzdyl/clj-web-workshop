f28c1b79703d0c1f9ced07aaa6313d2a24288709
Piotrek Bzdyl <p.bzdyl@smartrecruiters.com>
2016-06-07 08:45:13 +0200

    Exercise 4 setup.

---
+++ src/workshop/demo1.clj
@@ -0,0 +1,91 @@
+(ns workshop.demo1
+  (:require
+    [workshop.utils :refer :all]
+    [workshop.exercise4 :as shortener]
+    [workshop.memdb :as db]
+    [ring.util.http-response :as rs]
+    [compojure.api.sweet :refer :all]
+    [schema.core :as s]))
+
+(s/defschema TaskContent
+  {:content s/Str
+   :done    s/Bool})
+
+(s/defschema Task
+  {:id      s/Str
+   :content s/Str
+   :done    s/Bool})
+
+(s/defschema TaskCompletion
+  {:done s/Bool})
+
+(s/defschema TaskShortUrl
+  {:fullUrl s/Str
+   :shortUrl s/Str})
+
+(def handler
+  (api
+    {:swagger
+     {:ui   "/api-docs"
+      :spec "/swagger.json"
+      :data {:info {:title       "CLJ Web Workshop API"
+                    :description "Simple REST API"}
+             :tags [{:name "API", :description "Demo API"}]}}}
+
+    (context "/api" []
+      :tags ["API"]
+
+      (GET "/tasks" []
+        :return [Task]
+        :summary "Returns all tasks"
+
+        (rs/ok (db/fetch-tasks)))
+
+      (POST "/tasks" []
+        :return Task
+        :body [task TaskContent]
+        :summary "Creates a new task"
+
+        (rs/created (db/create-task! task)))
+
+      (GET "/tasks/:id" []
+        :return Task
+        :path-params [id :- s/Str]
+        :summary "Returns a task by ID"
+
+        (if-let [task (db/fetch-task id)]
+          (rs/ok task)
+          (rs/not-found)))
+
+      (PUT "/tasks/:id" []
+        :return Task
+        :path-params [id :- s/Str]
+        :body [task TaskContent]
+        :summary "Updates a task with specified ID"
+
+        (rs/ok (db/update-task! id task)))
+
+      (DELETE "/tasks/:id" []
+        :path-params [id :- s/Str]
+        :summary "Deletes a task with specified ID"
+
+        (db/delete-task! id)
+        (rs/no-content))
+
+      (PUT "/tasks/:id/completion" []
+        :return Task
+        :path-params [id :- s/Str]
+        :body [completion TaskCompletion]
+        :summary "Sets tasks done status to the provided value"
+
+        (rs/ok (db/set-task-done-status! id (:done completion))))
+
+      (POST "/tasks/:id/short-url" request
+        :return TaskShortUrl
+        :path-params [id :- s/Str]
+        :summary "Creates a short URL to a task with provided ID"
+
+        (let [full-url (build-task-url  request id)
+              short-url (shortener/shorten-url! full-url)]
+          (rs/ok {:fullUrl full-url
+                  :shortUrl short-url}))))))
---
+++ src/workshop/exercise4.clj
@@ -0,0 +1,18 @@
+(ns workshop.exercise4
+  (:require
+    [clj-http.client :as http]
+    [workshop.utils :refer :all]))
+
+;; Exercise goals
+;; shorten-url! function should use Google URL Shortener API to shorten the provided long-url.
+;; The function should return just the short URL, not the whole Google URL Shortener response.
+
+;; https://developers.google.com/url-shortener/v1/getting_started
+;; https://github.com/dakrone/clj-http
+;;
+;; curl https://www.googleapis.com/urlshortener/v1/url?key=your-api-key -H 'Content-Type: application/json' -d '{"longUrl": "http://www.google.com/"}'
+
+(def api-key "AIzaSyD8Pmu_34AgApBYaxW0LRFe7-wX7qKAnTw")
+
+(defn shorten-url! [long-url]
+  (implement-me!))
